c7e1e68175f3fc3ca90c0d0644e45fe5518a9aa5
ademeulenaere <Alex@alexdemeulenaere.cable.rcn.com>
2014-08-18 20:16:27 -0500

    3rd

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-	function getItem() {
-		$('#add').keydown(function (enter) {
-			if (enter.keyCode == 13) {
-				postItem();
-			}
-		})
-	}
+	$('#add').keydown(function (enter) {
+		if (enter.keyCode == 13) {
+			postItem();
+		}
+	});
+
+	$('items').on('click', 'p', function(e) {
+		$(this).toggleClass('strike');
+	});
+
+	$(this).remove();
 })
-	function postItem() {
-		var the_text = $('#add').val();
-		var the_item = $('<div class="items"></div>').text(the_text);
-	}
+
+function postItem() {
+	var the_text = $('#add').val();
+	var the_item = $('.items').append('<p class="text">' + the_text + '</p>');
+}
